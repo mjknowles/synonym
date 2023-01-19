969fb938e1f07d760e3c7d0f304d3f04faa879dd
mjknowles <mjknowles23@gmail.com>
2023-01-19 20:01:54 +0000

    Default UI exists

---
+++ src/components/AnswerInput.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const answer = ref("Enter a potential synonym");
+</script>
+
+<template>Answer: <input v-model="answer" /></template>
---
+++ src/views/GameView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import AnswerInput from "../components/AnswerInput.vue";
+</script>
+
+<template>
+  <h1>Synonym</h1>
+  <AnswerInput />
+</template>
